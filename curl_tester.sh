55e787bf5ebfdcf15c28646f4adb1d99458f73e8
中　翔 <shimajima-eiji@live.jp>
2022-01-20 07:53:28 +0900

    開発完了

--- curl_tester.sh
+++
@@ -1,5 +0,0 @@
-#/bin/sh
-
-### curlで動いているかどうかを調べるためのテスター
-### curl -sf https://raw.githubusercontent.com/shimajima-eiji/__Debug_Github/main/curl_tester.sh | sh -s
-echo "$SHELL $0"  # ${{ secrets.test }}を呼び出すことはできないので、あらかじめ.envなりに書いておいた方が無難
